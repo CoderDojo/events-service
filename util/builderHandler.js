ade41d5e4f081002afded79e8a8c4eb52b675f8f
Wardormeur <wardormeur@gmail.com>
2018-03-14 17:15:19 +0000

    Add support for ordering

--- util/builderHandler.js
+++ util/builderHandler.js
@@ -1,7 +1,7 @@
 module.exports = Model => (req, res, next) => {
   const qb = Model.query();
   if (req.query.orderBy) {
-    qb.orderBy(req.query.orderBy);
+    qb.orderBy(req.query.orderBy, req.query.direction || 'asc');
   }
   if (req.query.fields) {
     qb.column(req.query.fields.split(','));
