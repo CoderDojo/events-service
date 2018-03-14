5b4f45bad04e47ff255ecbb785e68910384bed84
Daniel Brierton <daniel@brierton.me>
2018-03-14 11:42:51 +0000

    Integration tests for listing events (#7)

--- test/integration/db/setup.js
+++ test/integration/db/setup.js
@@ -55,7 +55,35 @@ before(async () => {
     id: '3ae8fc05-55b6-4ea1-ad85-4f385452f764',
     dojoId: '95b351c7-8228-4a9d-8fcc-b6b8bf2dd0c2',
     dates: [
+      { startTime: '2018-04-25T10:00:00.000Z', endTime: '2018-04-25T12:00:00.000Z' },
+    ],
+  });
+  await db('cd_events').insert({
+    id: '0e83d8e7-b991-4e4e-b3bd-36aa956f6754',
+    dojoId: '95b351c7-8228-4a9d-8fcc-b6b8bf2dd0c2',
+    dates: [
       { startTime: '2018-05-02T10:00:00.000Z', endTime: '2018-05-02T12:00:00.000Z' },
+    ],
+  });
+  await db('cd_events').insert({
+    id: '84c0310e-49ff-4607-99da-a5abb9fb5641',
+    dojoId: '95b351c7-8228-4a9d-8fcc-b6b8bf2dd0c2',
+    dates: [
+      { startTime: '2018-05-09T10:00:00.000Z', endTime: '2018-05-09T12:00:00.000Z' },
+    ],
+  });
+  await db('cd_events').insert({
+    id: 'bcef18f8-b5ff-43a9-bc2c-7109f6e5dc20',
+    dojoId: '95b351c7-8228-4a9d-8fcc-b6b8bf2dd0c2',
+    dates: [
+      { startTime: '2018-05-16T10:00:00.000Z', endTime: '2018-05-16T12:00:00.000Z' },
+    ],
+  });
+  await db('cd_events').insert({
+    id: '072658b7-cabd-4e31-959b-756b65dec760',
+    dojoId: '95b351c7-8228-4a9d-8fcc-b6b8bf2dd0c2',
+    dates: [
+      { startTime: '2018-05-23T10:00:00.000Z', endTime: '2018-05-23T12:00:00.000Z' },
     ],
   });
 
