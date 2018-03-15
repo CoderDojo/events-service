3bb06d244a9a214aa32a828a5cb6f27ecca594f3
Wardormeur <wardormeur@gmail.com>
2018-03-15 17:30:46 +0000

    Use pino to log to logentries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('./util/pino-stream');
 const events = require('./events/routes');
 const sessions = require('./sessions/routes');
 const tickets = require('./tickets/routes');
@@ -12,11 +13,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+process.on('unhandledRejection', logger.error);
+process.on('uncaughtException', logger.error);
+app.use((err, req, res, next) => {
+  logger.error(err);
+  next();
+});
+
 app.use('/events', events);
 app.use('/sessions', sessions);
 app.use('/tickets', tickets);
 app.get('/ping', (req, res) => res.send(204));
 
-app.listen(3000, () => console.log('event-service listening on port 3000'));
+app.listen(3000, () => logger.info('event-service listening on port 3000'));
 
 module.exports = app;
--- util/ValidationHelper.js
+++ util/ValidationHelper.js
@@ -1,9 +1,11 @@
 const { checkSchema, validationResult } = require('express-validator/check');
+const logger = require('./pino-stream');
 
 class ValidationHelper {
   static handleErrors(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      logger.error(errors.mapped());
       return res.status(400).json({ errors: errors.mapped() });
     }
     return next();
