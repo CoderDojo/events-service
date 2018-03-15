bd0359d07e038d9c61cb14b18cebb85eb3cf9ea7
Wardormeur <wardormeur@gmail.com>
2018-03-15 18:14:04 +0000

    Add newrelic

---
+++ newrelic.js
@@ -0,0 +1,32 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name: ['events-service'],
+  agent_enabled: '',
+  license_key: '',
+  logging: {
+    level: 'info',
+  },
+  transaction_tracer: {
+    record_sql: 'obfuscated',
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*',
+    ],
+  },
+};
