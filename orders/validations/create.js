25bd6c541200bad27d56f81e7592b194593adcdd
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-05-08 12:14:09 +0100

    fix applications within orders test

--- orders/validations/create.js
+++ orders/validations/create.js
@@ -5,13 +5,7 @@ const applicationsValidator = require('../../applications/validations/create');
 module.exports = [
   body('userId', 'Invalid format').isUUID(),
   body('applications', 'Must have at least one application for an order').isLength({ min: 1 }),
-  // body('applications').custom(applicationsValidator.favoriteBook('hello')),
-  body('applications').custom((value, { req }) => {
-    if (value.length > 0) {
-      // console.log('got here');
-      applicationsValidator.checkApplications('applications.*.', body);
-    }
-  }),
+  applicationsValidator.checkApplications('applications.*.'),
   body('eventId', 'Invalid format').isUUID(),
   ValidationHelper.handleErrors,
 ];
