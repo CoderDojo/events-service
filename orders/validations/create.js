071e13f390074c1b056c67fb075dadc29cb43b2b
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-05-08 14:55:34 +0100

    use spread for checkApplications function

--- orders/validations/create.js
+++ orders/validations/create.js
@@ -5,7 +5,7 @@ const applicationsValidator = require('../../applications/validations/create');
 module.exports = [
   body('userId', 'Invalid format').isUUID(),
   body('applications', 'Must have at least one application for an order').isLength({ min: 1 }),
-  applicationsValidator.checkApplications('applications.*.'),
+  ...applicationsValidator.checkApplications('applications.*.'),
   body('eventId', 'Invalid format').isUUID(),
   ValidationHelper.handleErrors,
 ];
