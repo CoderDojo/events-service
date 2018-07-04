5656e10425aae56c1396c8e43edb549591499c6c
Wardormeur <wardormeur@gmail.com>
2018-07-04 11:34:22 +0100

    Add very important reporter

--- test/unit/orders/utils.spec.js
+++ test/unit/orders/utils.spec.js
@@ -1,16 +1,17 @@
-const { formatApplications } = require('../../../orders/utils');
+const { formatApplications, quantityByTicket } = require('../../../orders/utils');
 
 describe('orders/utils', () => {
   describe('formatApplications', () => {
     const event = {
       id: 'event1',
+      dojoId: 'dojo1',
       tickets: [
         { id: 't1', name: 'ticket1', type: 'mentor' },
         { id: 't2', name: 'ticket2', type: 'mentor' },
         { id: 't3', name: 'ticket3', type: 'mentor' },
       ],
     };
-    test('formatApplications should enforce ticketName, ticketType, dojoId and eventId', () => {
+    test('should enforce ticketName, ticketType, dojoId and eventId', () => {
       const applications = [{
         name: 'batman',
         dob: '2018-01-01',
@@ -22,9 +23,10 @@ describe('orders/utils', () => {
       expect(formatted.length).toEqual(1);
       expect(formatted[0].ticketName).toEqual('ticket1');
       expect(formatted[0].ticketType).toEqual('mentor');
-      expect(formatted[0].ticketName).toEqual('ticket1');
+      expect(formatted[0].dojoId).toEqual('dojo1');
+      expect(formatted[0].eventId).toEqual('event1');
     });
-    test('formatApplications should throw an error', () => {
+    test('should throw an error', () => {
       const applications = [{
         name: 'batman',
         dob: '2018-01-01',
@@ -34,9 +36,30 @@ describe('orders/utils', () => {
       }];
       expect(() => formatApplications(event, applications)).toThrow('Invalid ticket sent');
     });
-    test('formatApplications should return an empty array if by any chance, there is no applications', () => {
+    test('should return an empty array if by any chance, there is no applications', () => {
       const applications = [];
       expect(formatApplications(event, applications)).toEqual([]);
+    });
+  });
+  describe('quantityByTicket', () => {
+    test('should return the number of application by ticketId', () => {
+      const applications = [{
+        name: 'batman',
+        dob: '2018-01-01',
+        ticketId: 'fakeTicket',
+        ticketName: 'tickettack',
+        ticketType: 'superhero',
+      }, {
+        name: 'batman',
+        dob: '2018-01-01',
+        ticketId: 'fakeTicket',
+        ticketName: 'tickettack',
+        ticketType: 'superhero',
+      }];
+      const expected = {
+        fakeTicket: 2,
+      };
+      expect(quantityByTicket(applications)).toEqual(expected);
     });
   });
 });
