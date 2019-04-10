435d799cab7204f6f7579dc752736bcf4a60c593
Wardormeur <wardormeur@gmail.com>
2019-04-10 16:29:00 +0100

    Ensure an empty calendar ICS is returned (#29)
    When there is no events

--- test/unit/events/formatter.spec.js
+++ test/unit/events/formatter.spec.js
@@ -29,6 +29,20 @@ describe('EventFormatter', () => {
       expect(res).to.match(/DESCRIPTION:https:\/\/zen\.coderdojo\.com\/events\/1/);
       expect(res).to.match(/URL:https:\/\/zen\.coderdojo.com\/api\/3\.0\/events\/1\.ics/);
     });
+    it('should format multiple events into an ics', () => {
+      const results = [{
+        id: 1,
+        startTime: new Date(),
+        endTime: new Date(),
+      }, {
+        id: 2,
+        startTime: new Date(),
+        endTime: new Date(),
+      }];
+      const res = formatter.toICS({ results });
+      const events = res.split('VEVENT');
+      expect(events.length).to.equal(5);
+    });
     it('should add the geopoint if the event has some', () => {
       const result = {
         id: 1,
@@ -38,6 +52,17 @@ describe('EventFormatter', () => {
       };
       const res = formatter.toICS(result);
       expect(res).to.match(/GEO:1.1;2.2/);
+    });
+    it('should return an empty calendar', () => {
+      const result = { results: [] };
+      const res = formatter.toICS(result);
+      expect(res).to.equal(`BEGIN:VCALENDAR\r
+VERSION:2.0\r
+CALSCALE:GREGORIAN\r
+PRODID:coderdojo/zen\r
+METHOD:PUBLISH\r
+X-PUBLISHED-TTL:PT1H\r
+END:VCALENDAR`);
     });
   });
 });
